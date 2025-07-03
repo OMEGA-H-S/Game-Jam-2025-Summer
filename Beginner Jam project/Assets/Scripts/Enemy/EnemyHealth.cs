82a648e1c0757f3991ecf6babc6dea1bb43fddbe
OMEGA-H-S <132601115+OMEGA-H-S@users.noreply.github.com>
2025-07-03 04:44:52 -0400

    Revert "Revert "Merge branch 'main' into OMEGA_H-final-branch-hopefully""

--- Beginner Jam project/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Beginner Jam project/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -32,7 +32,7 @@ public class EnemyHealth : MonoBehaviour
         else
         {
             color = "Boss";
-            enemyHealth = 100;
+            enemyHealth = 80;
         }
 
         anim = GetComponent<Animator>();
